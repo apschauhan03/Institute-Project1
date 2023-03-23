5c497d13dc2abaeb609972620417e18b6f61a628
Abhijit Singh <abhijitchau8948@gmail.com>
2023-03-23 21:36:51 +0530

    added css to home

--- src/components/Home/Corousel/slide.jsx
+++ src/components/Home/Corousel/slide.jsx
@@ -1,18 +1,19 @@
 import React from 'react'
-// import "./slide.css"
+import defaultIMG from './157347.png'
+import './slide.css';
 export default function slide() {
   return (
     <div className='slidecontainer'>
       <div id="carouselExampleFade" className="carousel slide carousel-fade">
   <div className="carousel-inner">
     <div className="carousel-item active">
-      <img src="https://cdn.pixabay.com/photo/2020/07/02/04/31/matrix-5361690_1280.png" className="d-block w-100" alt="..." />
+      <img src={defaultIMG} className="d-block w-100" alt="..." />
     </div>
     <div className="carousel-item">
-      <img src="https://cdn.pixabay.com/photo/2019/03/03/14/38/hacker-4031973_1280.jpg" className="d-block w-100" alt="..." />
+      <img src={defaultIMG} className="d-block w-100" alt="..." />
     </div>
     <div className="carousel-item">
-      <img src="https://cdn.pixabay.com/photo/2017/05/10/12/41/hacker-2300772_1280.jpg" className="d-block w-100" alt="..." />
+      <img src={defaultIMG} className="d-block w-100" alt="..." />
     </div>
   </div>
   <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
--- src/navbar.jsx
+++ src/navbar.jsx
@@ -1,23 +1,25 @@
 import React, { useState } from "react";
 import { BrowserRouter, Route, Link } from "react-router-dom";
-import './navbar.css'
+import "./navbar.css";
 function Navbar() {
   return (
     <nav className="navbar">
-      <ul className="list">
-        <li className="navbar-element Logo">
-          <Link to="/">CyberKing</Link>
-        </li>
-        <li className="navbar-element">
-          <Link to="/blogs">Blogs</Link>
-        </li>
-        <li className="navbar-element">
-          <Link to="/courses">Courses</Link>
-        </li>
-        <li className="navbar-element">
-          <Link to="/contact">Contact</Link>
-        </li>
-      </ul>
+      <div className="navbar-heading">
+        <Link to="/">CyberKing</Link>
+      </div>
+      <div className="navright">
+        <ul className="list">
+          <li className="navbar-element">
+            <Link to="/blogs">Blogs</Link>
+          </li>
+          <li className="navbar-element">
+            <Link to="/courses">Courses</Link>
+          </li>
+          <li className="navbar-element">
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 }
